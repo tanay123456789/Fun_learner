ccdf4214db753f1e0a980eed1195ec82918a4a41
Tanay <tanaydwivedi456@gmail.com>
2021-09-04 22:19:41 +0530

    added pink box

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import AndroidRoadmap from './components/roadmap/AndroidRoadmap';
-
+import Start from './components/Start';
 
 import './App.css';
 
 function App() {
   return (
+    <div>
     <AndroidRoadmap/>
-    
+    <Start/>
+    </div>
   );
 }
 
---
+++ my-app/src/components/Start.js
@@ -0,0 +1,12 @@
+import React from 'react'
+/*import '../App.css'*/
+
+const Start=()=>{
+    return(
+    <div className="start-style">
+    
+    <p>Where to start</p>
+    </div>
+    )}
+    
+export default Start;
