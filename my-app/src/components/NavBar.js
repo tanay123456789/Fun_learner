a17a5941e175bc5ca463f1758e2fb9623e5bb527
Tanay <tanaydwivedi456@gmail.com>
2021-09-04 17:50:40 +0530

    fix:missing return statement

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,12 +1,16 @@
-
+import React from "react";
 const NavBar=()=>{
-    <div class="topnav">
+  return(
+  <div class="topnav">
   <a class="active" href="#home">Home</a>
   <a href="#news">News</a>
   <a href="#contact">Contact</a>
   <a href="#about">About</a>
 </div>
 
+
+)
+    
 
 }
 export default NavBar;
